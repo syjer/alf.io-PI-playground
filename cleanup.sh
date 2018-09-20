5246a7ee5e217ee69137fe8613f17d38055ef0a4
Sylvain Jermini <sylvain.jermini@syjer.com>
2018-09-20 16:41:58 +0200

    initial commit for alf.io-pi playground

---
+++ cleanup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -f a{1,2}/alfio.{data,log,properties,script}
+rm -f a{1,2}/alfio-pi-synckv
+rm -rf a{1,2}/{alfio.tmp,logs}
+
---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp ../alf.io-PI/backend/build/libs/alfio-pi-1.0.0-SNAPSHOT.jar a1
+cp ../alf.io-PI/backend/build/libs/alfio-pi-1.0.0-SNAPSHOT.jar a2
