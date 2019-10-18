d3721f2b05c0cc9bc36e2ba16afe4c6b1df1c9b3
Sylvain Jermini <sylvain.jermini@syjer.com>
2019-10-18 15:35:43 +0200

    update playground

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp ../alf.io-PI/backend/build/libs/alfio-pi-1.0.0-SNAPSHOT.jar a1
-cp ../alf.io-PI/backend/build/libs/alfio-pi-1.0.0-SNAPSHOT.jar a2
+cp ../alf.io-PI/backend/build/libs/alfio-pi-*.jar a1
+cp ../alf.io-PI/backend/build/libs/alfio-pi-*.jar a2
